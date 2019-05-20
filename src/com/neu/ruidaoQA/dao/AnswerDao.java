062adcde65e69aa5feb013f4c4a2c587b3435bb8
swc_20180064 <123@qq.com>
2019-05-20 17:35:17 +0800

    txytest

--- src/com/neu/ruidaoQA/dao/AnswerDao.java
+++ src/com/neu/ruidaoQA/dao/AnswerDao.java
@@ -1,12 +1,9 @@
 package com.neu.ruidaoQA.dao;
 
 public interface AnswerDao {
-<<<<<<< HEAD
-	
-=======
 
 	//fafafewarerere
 //hbhjbjhb
 	//fafa
->>>>>>> branch 'master' of https://github.com/XiangyuTang/RuiDaoQA.git
+
 }
