3fc5a8f72fe35cba24a55f86b9fd5a43d71c66b3
40947 <40947@DESKTOP-I08EB6N>
2019-05-22 14:04:43 +0800

    1.userinfo.html的美观化 2.QuestionDao的部分添加

--- src/com/neu/ruidaoQA/dao/QuestionDao.java
+++ src/com/neu/ruidaoQA/dao/QuestionDao.java
@@ -1,5 +1,8 @@
 package com.neu.ruidaoQA.dao;
 
+import com.neu.ruidaoQA.entity.Question;
+
 public interface QuestionDao {
+	public int addQuestion(Question q);
 	
 }
--- src/com/neu/ruidaoQA/dao/impl/QuestionDaoimpl.java
+++ src/com/neu/ruidaoQA/dao/impl/QuestionDaoimpl.java
@@ -2,7 +2,19 @@ package com.neu.ruidaoQA.dao.impl;
 
 import com.neu.ruidaoQA.dao.QuestionDao;
 import com.neu.ruidaoQA.dbutil.BaseDao;
+import com.neu.ruidaoQA.entity.Question;
 
 public class QuestionDaoimpl extends BaseDao implements QuestionDao{
+	public int addQuestion(Question q) {
+		Integer question_id=q.getQuestion_id();
+		Integer user_id=q.getUser_id();
+		Integer question_type=q.getQues_type_id();
+		String question_content=q.getContent();
+		Integer collect_number=q.getCollect_num();
+		
+		
+		return 0;
+		
+	}
 
 }
