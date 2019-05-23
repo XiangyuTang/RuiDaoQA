0428f16d921048778296d15fcf69051c34add0e9
swc_20180064 <123@qq.com>
2019-05-23 14:52:47 +0800

    更新：answer后端函数

--- src/com/neu/ruidaoQA/dao/AnswerDao.java
+++ src/com/neu/ruidaoQA/dao/AnswerDao.java
@@ -6,4 +6,5 @@ public interface AnswerDao {
 	int addAcclaim_number(int answer_id);//增加相应回答点赞数
 	int addDefame_number(int answer_id);//增加相应回答踩数
 	int addAnswer(Answer ans);//增加一行question的answer
+	
 }
