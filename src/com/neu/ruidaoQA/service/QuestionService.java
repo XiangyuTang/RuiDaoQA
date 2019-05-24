3e14c0ea5c9a0889963b2d323e89057013e12cb6
40947 <40947@DESKTOP-I08EB6N>
2019-05-24 20:59:52 +0800

    完成用户界面的问题列表功能，其他功能在实现中

--- src/com/neu/ruidaoQA/service/QuestionService.java
+++ src/com/neu/ruidaoQA/service/QuestionService.java
@@ -1,6 +1,7 @@
 package com.neu.ruidaoQA.service;
 
 import java.sql.Date;
+import java.util.ArrayList;
 
 import com.neu.ruidaoQA.entity.Question;
 
@@ -9,4 +10,5 @@ public interface QuestionService {
 	public Question createQuestion(Integer question_id, Integer user_id, Integer ques_type_id, String content, Integer collect_num,
 			Integer answer_num, Date publish_time,String ques_title);
 	public Question getQuestion(int question_id);
+	public ArrayList<Question> getQuestionLists(int user_id);
 }
