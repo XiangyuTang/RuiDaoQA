415aa83c235f85edda7f83cd07a7dbd5b8ce6b9c
18940 <18940@DESKTOP-NOMLVQ0>
2019-05-25 22:32:11 +0800

    交喽

--- src/com/neu/ruidaoQA/servlet/detail_question/toDetailQuesServlet.java
+++ src/com/neu/ruidaoQA/servlet/detail_question/toDetailQuesServlet.java
@@ -26,18 +26,18 @@ public class toDetailQuesServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		Integer question_id = Integer.parseInt(request.getParameter("question_id"));
+//		Integer question_id = Integer.parseInt(request.getParameter("question_id"));
 		QuestionServiceimpl qsip = new QuestionServiceimpl();
 		UserServiceimpl usip = new UserServiceimpl();
 		AnswerServiceimpl answerServiceimpl = new AnswerServiceimpl();
 		List<Answer> answers = answerServiceimpl.getAnswerslist(2);
-		Question q = qsip.getQuestion(question_id);
+		Question q = qsip.getQuestion(2);
 		Integer user_id = q.getUser_id();
 		User u = usip.getUser(user_id);
 		request.setAttribute("User", u);
 		request.setAttribute("Question", q);
 		request.setAttribute("answerlist", answers);
-		System.out.println(q.getContent());		
+		System.out.println(q.getContent());	
 		request.getRequestDispatcher("detailQues.jsp").forward(request, response);
 		
 	}
