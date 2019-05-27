af9ff3e94a64a5b4ab3d886f766d92eeefda445b
18940 <18940@DESKTOP-NOMLVQ0>
2019-05-27 13:44:19 +0800

    小修改

--- src/com/neu/ruidaoQA/servlet/detail_question/getQuestionServlet.java
+++ src/com/neu/ruidaoQA/servlet/detail_question/getQuestionServlet.java
@@ -47,23 +47,24 @@ public class getQuestionServlet extends HttpServlet implements java.io.Serializa
 					"<div class='question-info'><span class='question-answer-num'>"+q.getAnswer_num()+"回答</span> <span class='question-follow-num'>"+q.getCollect_num()+"人收藏</span></div>"+
 					"<div class='detail-about'>"+
 						"<a class='jie-user' href=''> <img "+
-							"src='images/uer.jpg' alt='头像'> <cite> 压缩"+
-								"<em>2017-05-01发布</em> </cite> </a>"+
+							"src='"+q.getAnswer().getUser().getHead_photo()+"' alt='头像'> <cite> "+q.getAnswer().getUser().getNick_name()+" "+
+								"<em>"+q.getAnswer().getPublish_time()+"</em> </cite> </a>"+
 						"<div class='detail-hits' data-id='{{rows.id}}'>"+
+						"<input type='hidden' name='user_id' value='"+q.getAnswer().getUser().getUser_id()+"' /><span></span>"+
 							"<button class='layui-btn layui-btn-radius layui-btn-sm' style='width:80px; border-radius:20px;'>关注</button>"+
 						"</div>"+
 					"</div>"+
 					"<div class='detail-body photos' style='margin-bottom: 0px;'>"+
-						"<p>"+q.getContent()+
+						"<p>"+q.getAnswer().getContent()+
 						"</p>"+
 					"</div>"+
 					"<div class='jieda-reply'>"+
-							"<input type='hidden' name='answer_id' value='2'/>"+
+							"<input type='hidden' name='answer_id' value='"+q.getAnswer().getAnswer_id()+"'/>"+
 							"<span class='jieda-zan zanok' type='zan'>"+
-								"<i class='layui-icon layui-icon-praise' style='font-size: 20px; color: #009688;' title='赞'></i><em style='font-size: 15px; color: #009688;'>"+12+"</em>"+
+								"<i class='layui-icon layui-icon-praise' style='font-size: 20px; color: #009688;' title='赞'></i><em style='font-size: 15px; color: #009688;'>"+q.getAnswer().getDianzan_num()+"</em>"+
 							"</span>"+
 							"<span class='jieda-zan zanok' type='zan'><i "+
-								"class='layui-icon layui-icon-tread' style='font-size: 20px; color: #009688;' title='踩'></i><em style='font-size: 15px; color: #009688;'>3</em>"+
+								"class='layui-icon layui-icon-tread' style='font-size: 20px; color: #009688;' title='踩'></i><em style='font-size: 15px; color: #009688;'>"+q.getAnswer().getCai_num()+"</em>"+
 							"</span>"+
 							/*"<span class='jieda-zan zanok' type='zan'><i "+
 								"class='layui-icon layui-icon-reply-fill'  style='font-size: 20px; color: #009688;' title='评论'></i><em style='font-size: 15px; color: #009688;'>"+q.getAnswer_num()+"</em>"+
